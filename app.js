eff74148eadd12acb6c932dccc7e5e124023240f
webfaisalbd <web.faisal.bd@gmail.com>
2022-06-17 13:11:16 +0600

    product info get and show

--- app.js
+++ app.js
@@ -22,6 +22,42 @@ const handleUserInfo = document.getElementById('handleUserInfo').addEventListene
 })
 
 
+
+
+// product info get and show 
+
+const handleProductInfo = document.getElementById('handleProductInfo').addEventListener('click',function(){
+    //  for taking input 
+    const itemNameInput = document.getElementById('item-name-input').value;
+    const itemPriceInput = document.getElementById('item-price-input').value;
+    const itemQuantityInput = document.getElementById('item-quantity-input').value;
+    // console.log(itemNameInput, itemPriceInput, itemQuantityInput);
+
+    // for showing input 
+    const itemNameOutput = document.getElementById('item-name-output');
+    const itemPriceOutput = document.getElementById('item-price-output');
+    const itemQuantityOutput = document.getElementById('item-quantity-output');
+    itemNameOutput.innerText = itemNameInput;
+    itemPriceOutput.innerText = itemPriceInput;
+    itemQuantityOutput.innerText = itemQuantityInput;
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // current date 
 const todayDate = new Date();
 const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
