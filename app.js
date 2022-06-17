1bf47c9ad47d285d5673ced113f283f364f26c55
webfaisalbd <web.faisal.bd@gmail.com>
2022-06-17 12:40:38 +0600

    name,email,phone set and get

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
-function handleUserInfoFunction () {
+// function handleUserInfoFunction () {
 
-    const data = document.getElementById('name-input');
-    console.log(data.value);
+//     const data = document.getElementById('name-input');
+//     console.log(data.value);
 
-}
+// }
+
+const handleUserInfo = document.getElementById('handleUserInfo').addEventListener('click',function(){
+    //  for taking input 
+    const nameInput = document.getElementById('name-input').value;
+    const emailInput = document.getElementById('email-input').value;
+    const phoneInput = document.getElementById('phone-input').value;
+    // console.log(nameInput, emailInput, phoneInput);
+
+    // for showing input 
+    const nameOutput = document.getElementById('name-output');
+    const emailOutput = document.getElementById('email-output');
+    const phoneOutput = document.getElementById('phone-output');
+    nameOutput.innerText = nameInput;
+    emailOutput.innerText = emailInput;
+    phoneOutput.innerText = phoneInput;
+})
